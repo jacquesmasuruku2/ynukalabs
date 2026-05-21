1deaa330b228e3d19164607480cbdcab6fa3b60d
Jacques Masuruku <jacquesmasuruku2@gmail.com>
2026-05-21 12:21:20 +0200

    feat: redirect admin root to login page

--- ynuka-admin/src/routes/index.tsx
+++ ynuka-admin/src/routes/index.tsx
@@ -2,7 +2,7 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   beforeLoad: () => {
-    throw redirect({ to: "/admin" });
+    throw redirect({ to: "/login" });
   },
   component: () => null,
 });
