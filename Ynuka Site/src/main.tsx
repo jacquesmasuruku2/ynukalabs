daa696a57f039053245c5512ee9320cb3729548b
Jacques Masuruku <jacquesmasuruku2@gmail.com>
2026-05-22 13:59:58 +0200

    refactor: convert Admin Ynuka from TanStack Start SSR to SPA for Interserver hosting

--- Ynuka Site/src/main.tsx
+++ Ynuka Site/src/main.tsx
@@ -10,7 +10,12 @@ import "@fontsource/sn-pro/700.css";
 import "@fontsource/sn-pro/800.css";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error('Failed to find element with id "root". Make sure index.html has <div id="root"></div>');
+}
+
+createRoot(rootElement).render(
   <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
     <App />
   </ThemeProvider>
